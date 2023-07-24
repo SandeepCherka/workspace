1e9179f5a8a063d232ab08407b96612db69c0deb
root <root@ip-172-31-89-229.ec2.internal>
2023-07-24 12:26:55 +0000

    commiting

---
+++ mono-project/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+region = "ap-south-1"
+access_key = "AKIAZUZOHD2I7HNXAEVY"
+secret_key = "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"
+
+}
+
+locals {
+instance_name = "${terraform.workspace}-instance"
+}
+resource "aws_instance" "one" {
+ami = "ami-006935d9a6773e4ec"
+instance_type = "t2.micro"
+key_name = "mumbaikey"
+vpc_security_group_ids = [aws_security_group.two.id]
+tags = {
+Name = local.instance_name
+}
+}
